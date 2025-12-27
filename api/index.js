652381a9721d5a8b8931c44bd02d8b90a8e94a4b
Harshsingla15 <harshsingla649@gmail.com>
2025-12-27 11:53:39 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully!");
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error", err);
+    process.exit(1);
+  });
 
 const app = express();
 
