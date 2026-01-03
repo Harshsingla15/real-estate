150695f32eeee01596ea149f268a1908480cf13d
Harshsingla15 <harshsingla649@gmail.com>
2026-01-03 20:33:45 +0530

    implemented create listing api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import listingRoute from "./routes/listing.route.js";
 dotenv.config();
 
 mongoose
@@ -29,6 +30,7 @@ app.listen(PORT, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRoute);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
